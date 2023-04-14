9075c9991d7de317288ab93e20e17ac7d91bbbdc
mounesi <armsohi@gmail.com>
2023-04-14 04:52:09 +0000

    feat: adding random number and username length 22

--- number_guess.sh
+++ number_guess.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+echo -e "Enter your username:"
+read USERNAME
+
+NUMBER=$((1 + RANDOM % 100))
+
+if [ ${#USERNAME} -le 22];
+then
+  # echo accepted
+  echo accepted
+
+else
+  # echo is not accpted
+  exit 1
+fi
